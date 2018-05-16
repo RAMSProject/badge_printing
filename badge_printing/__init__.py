c62c8734bd4b904d4cdec2c6e5d685c567cddd29
Victoria Earl <kitsuta@gmail.com>
2018-05-16 11:32:55 -0400

    Fix base-admin

--- badge_printing/__init__.py
+++ badge_printing/__init__.py
@@ -3,6 +3,7 @@ from os.path import join
 from uber.jinja import template_overrides
 from uber.utils import mount_site_sections, static_overrides
 from .config import badge_print_config
+from . import models  # noqa: F401
 
 static_overrides(join(badge_print_config['module_root'], 'static'))
 template_overrides(join(badge_print_config['module_root'], 'templates'))
